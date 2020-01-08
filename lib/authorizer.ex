7d51ff9d14e0ec1f88a10d9e590905b6a89c99af
Carlo Gilmar <carlogilmar12@gmail.com>
2020-01-08 16:01:33 -0600

    Cleaning authorizer module

--- lib/authorizer.ex
+++ lib/authorizer.ex
@@ -1,18 +1,2 @@
 defmodule Authorizer do
-  @moduledoc """
-  Documentation for Authorizer.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Authorizer.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
