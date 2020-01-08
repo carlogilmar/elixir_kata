e4d32e1e11f28048cce480f1e216395c0a9331e2
Carlo Gilmar <carlogilmar12@gmail.com>
2020-01-08 16:25:11 -0600

    Agregando validacion de cuanta inicializada

--- lib/authorizer.ex
+++ lib/authorizer.ex
@@ -1,2 +1,9 @@
 defmodule Authorizer do
+    def valid_account_initialized({:not_initialized, _transaction}) do
+        {:error, :not_initialized, :not_initialized}
+    end
+
+    def valid_account_initialized({account, transaction}) do
+        {:ok, account, transaction}
+    end
 end
